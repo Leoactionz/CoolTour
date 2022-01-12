ad6e1c830b4c4816c869ebf79bf0ebc972e9ca37
LeotheUXBro <leoucu@gmail.com>
2022-01-12 21:41:50 +0100

    Updated bookingController.js 🚀

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,10 +12,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -61,7 +61,6 @@ const createBookingCheckout = async (session) => {
   await Booking.create({ tour, user, price });
 
   res.redirect(req.originalUrl);
-
 };
 
 exports.webhookCheckout = (req, res, next) => {
